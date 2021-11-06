b8c00c3eb24f942fbd9f30045111488edfb3eb06
simranlotey <simran642003@gmail.com>
2021-11-06 16:12:41 +0530

    added static

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
